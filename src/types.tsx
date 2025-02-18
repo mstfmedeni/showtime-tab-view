e449c37a0a5e00eff584a469cf5f9a5940eebae7
alantoa <toacncom@gmail.com>
2025-02-18 18:01:01 +0800

    fix: reset list position

--- src/types.tsx
+++ src/types.tsx
@@ -1,17 +1,20 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import type React from 'react'
-import type { ComponentClass } from 'react'
-import type { ScrollViewProps } from 'react-native'
+import type React from "react";
+import type { ComponentClass } from "react";
+import type { ScrollViewProps } from "react-native";
 
-import type { NativeGesture } from 'react-native-gesture-handler'
-import type { SharedValue } from 'react-native-reanimated'
-import type { TabViewProps, Route as TabViewRoute } from 'react-native-tab-view'
+import type { NativeGesture } from "react-native-gesture-handler";
+import type { SharedValue } from "react-native-reanimated";
+import type {
+  TabViewProps,
+  Route as TabViewRoute,
+} from "react-native-tab-view";
 
 export type Route = TabViewRoute & {
-  key?: string
-  index: number
-  subtitle?: string | number
-}
+  key?: string;
+  index: number;
+  subtitle?: string | number;
+};
 
 export enum RefreshTypeEnum {
   Idle = 0,
@@ -23,96 +26,101 @@ export enum RefreshTypeEnum {
 }
 
 export type CollapsibleHeaderProps<T extends Route> = {
-  initHeaderHeight?: number
-  renderScrollHeader?: () => React.ReactElement | null
-  overridenShareAnimatedValue?: SharedValue<number>
-  overridenTranslateYValue?: SharedValue<number>
-  initTabbarHeight?: number
-  minHeaderHeight?: number
-  overflowHeight?: number
-  headerRespond?: boolean
-  scrollEnabled?: boolean
-  isRefreshing?: boolean
-  onStartRefresh?: () => void
-  renderRefreshControl?: (refreshProps: RefreshControlProps) => React.ReactElement
-  refreshHeight?: number
-  overflowPull?: number
-  pullExtendedCoefficient?: number
-  animationHeaderPosition?: SharedValue<number>
-  animationHeaderHeight?: SharedValue<number>
-  panHeaderMaxOffset?: number
-  onPullEnough?: () => void
-  refreshControlColor?: string
-  refreshControlTop?: number
-  emptyBodyComponent?: JSX.Element | null
-  renderSceneHeader?: (props: T) => JSX.Element | null
+  initHeaderHeight?: number;
+  renderScrollHeader?: () => React.ReactElement | null;
+  overridenShareAnimatedValue?: SharedValue<number>;
+  overridenTranslateYValue?: SharedValue<number>;
+  initTabbarHeight?: number;
+  minHeaderHeight?: number;
+  overflowHeight?: number;
+  headerRespond?: boolean;
+  scrollEnabled?: boolean;
+  isRefreshing?: boolean;
+  onStartRefresh?: () => void;
+  renderRefreshControl?: (
+    refreshProps: RefreshControlProps
+  ) => React.ReactElement;
+  refreshHeight?: number;
+  overflowPull?: number;
+  pullExtendedCoefficient?: number;
+  animationHeaderPosition?: SharedValue<number>;
+  animationHeaderHeight?: SharedValue<number>;
+  panHeaderMaxOffset?: number;
+  onPullEnough?: () => void;
+  refreshControlColor?: string;
+  refreshControlTop?: number;
+  emptyBodyComponent?: JSX.Element | null;
+  renderSceneHeader?: (props: T) => JSX.Element | null;
   /**
    * Enabling this option will prevent Reanimated & GestureHandler from crashing sometimes in debug mode.
    */
-  enableGestureRunOnJS?: boolean
-}
+  enableGestureRunOnJS?: boolean;
+};
 
 export type TabViewCustomRenders = {
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  renderTabBarContainer: (children: any) => JSX.Element
+  renderTabBarContainer: (children: any) => JSX.Element;
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  renderSceneHeader: (children: any, props: any) => JSX.Element
-}
+  renderSceneHeader: (children: any, props: any) => JSX.Element;
+};
 
-export type GestureContainerProps<T extends Route> = Pick<TabViewProps<Route>, 'navigationState'> &
+export type GestureContainerProps<T extends Route> = Pick<
+  TabViewProps<Route>,
+  "navigationState"
+> &
   CollapsibleHeaderProps<T> & {
-    initialPage: number
-    renderTabView: (e: TabViewCustomRenders) => JSX.Element
-  }
+    initialPage: number;
+    renderTabView: (e: TabViewCustomRenders) => JSX.Element;
+  };
 
 export interface RefreshControlProps {
-  refreshValue: SharedValue<number>
-  refreshType: SharedValue<RefreshTypeEnum>
-  progress: SharedValue<number>
-  refreshControlColor?: string
+  refreshValue: SharedValue<number>;
+  refreshType: SharedValue<RefreshTypeEnum>;
+  progress: SharedValue<number>;
+  refreshControlColor?: string;
 }
 export type SceneProps<P extends object> = P & {
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  ContainerView: any
+  ContainerView: any;
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  forwardedRef: any
-  index: number
-  useExternalScrollView?: boolean
-} & ScrollViewProps
+  forwardedRef: any;
+  index: number;
+  useExternalScrollView?: boolean;
+} & ScrollViewProps;
 
 export type UpdateSceneInfoParams = {
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  scrollRef: any
-  index: number
-  scrollY: SharedValue<number>
-}
+  scrollRef: any;
+  index: number;
+  scrollY: SharedValue<number>;
+};
 
-export type ScrollableView<T> = ComponentClass<T>
+export type ScrollableView<T> = ComponentClass<T>;
 
 export type ForwardRefType<T> =
   | ((instance: T | null) => void)
   | React.MutableRefObject<T | null>
-  | null
+  | null;
 
 export type GesturePanContext = {
-  startY: SharedValue<number>
-  basyY: SharedValue<number>
-}
+  startY: SharedValue<number>;
+  basyY: SharedValue<number>;
+};
 export type TabHeaderContext = {
-  isSlidingHeader: SharedValue<boolean>
-  shareAnimatedValue: SharedValue<number>
-  isStartRefreshing: SharedValue<boolean>
-  minHeaderHeight: number
-  tabbarHeight: number
-  headerHeight: number
-  scrollStickyHeaderHeight: number
-  refreshHeight: number
-  overflowPull: number
-  pullExtendedCoefficient: number
-  headerTrans: SharedValue<number>
-  expectHeight: number
-  refHasChanged: (ref: NativeGesture) => void
-  curIndexValue: SharedValue<number>
-  updateSceneInfo: (e: UpdateSceneInfoParams) => void
-  scrollViewPaddingTop: number
-} | null
+  isSlidingHeader: SharedValue<boolean>;
+  shareAnimatedValue: SharedValue<number>;
+  isStartRefreshing: SharedValue<boolean>;
+  minHeaderHeight: number;
+  tabbarHeight: number;
+  headerHeight: number;
+  scrollStickyHeaderHeight: number;
+  refreshHeight: number;
+  overflowPull: number;
+  pullExtendedCoefficient: number;
+  headerTrans: SharedValue<number>;
+  expectHeight: number;
+  refHasChanged: (ref: NativeGesture) => void;
+  curIndexValue: SharedValue<number>;
+  updateSceneInfo: (e: UpdateSceneInfoParams) => void;
+  scrollViewPaddingTop: number;
+} | null;
